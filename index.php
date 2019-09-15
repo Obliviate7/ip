8964b13540b448df46f02a008f8690de567d04f2
Nick Damico <nick@nickdami.co>
2019-09-15 01:23:23 -0300

    test/perm

--- index.php
+++ index.php
@@ -28,27 +28,9 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><img src="img/IPlogo.png" width="50" height="50" alt=""> <h1>Integra Pymes SGR</h1> </a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">¿Qué es una SGR?</a></li>
-            <li><a href="#">Productos</a></li>
-            <li><a href="#">Ventajas</a></li>
-            <li><a href="#">Nosotros</a></li>
-            <li><a href="#">Contacto</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <?php
+  php_includes("components/nav.php")
+  ?>
 
     <div class="home-img row">
       <div class="col-md-12">
